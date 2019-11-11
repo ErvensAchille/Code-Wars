5b29993c51fba6e1e26d3bccd7a6e35fc40ec27e
Ervens Achille <55925419+ErvensAchille@users.noreply.github.com>
2019-11-11 13:50:34 -0500

    Add files via upload

---
+++ whatswrongwiththeseidentifiers.js
@@ -0,0 +1,8 @@
+// Can you amend this object so that its properties comprise only vaild identifiers?
+
+var Person = {
+  firstName: "John",
+  secondName: "Doe",
+  email: "john.doe@email.com",
+  gender: "Male"
+}
